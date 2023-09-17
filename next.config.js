62ce56b2377179c2c04abce65959bb6b0201c0c7
Paramas <paramas.w@livinginsider.com>
2023-09-17 19:01:38 +0700

    refactor: ♻️ redirect

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/gallery',
+        permanent: true,
+      },
+    ]
+  },
   images: {
     domains: ["lh3.googleusercontent.com", "s3.us-west-2.amazonaws.com"],
   },
